5aeae8e017e846384554ed8672f2cab303fed733
dexterlb <dexterlb@qtrp.org>
2017-06-04 12:11:19 +0300

    verify consistence of transducers

--- src/Main.hs
+++ src/Main.hs
@@ -14,6 +14,77 @@ import Data.Hashable (hashWithSalt, Hashable)
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
 
+updateEquiv :: Trans -> Trans
+updateEquiv Trans {states, start} = Trans {
+    states = states,
+    start = start,
+    equiv = HashSet.fromList $ HashMap.elems states
+}
+
+-- **** Prints ****
+
+instance Show Trans where
+    show = (foldr (++) "") . (map (++ "\n")) . showTransLines
+
+instance Show State where
+    show = (foldr (++) "") . (map (++ "\n")) . showStateLines
+
+showTransLines :: Trans -> [String]
+showTransLines t
+    =  (showTransDataLines t)
+    ++ (showVerifyEquivLines t)
+    ++ (showMismatchingStatesLines t)
+
+showTransDataLines :: Trans -> [String]
+showTransDataLines Trans {states, start}
+    =  ["transducer with start " ++ (show start)]
+    ++ (map ("  " ++) (foldr (++) [] (map showStateLines (HashMap.elems states))))
+
+showStateLines :: State -> [String]
+showStateLines State {sid, transition, final, output}
+    =  ["state " ++ show sid ++ ":"]
+    ++ ["  transitions:"]
+    ++ (map ("    " ++) (showMapLines transition))
+    ++ ["  outputs:"]
+    ++ (map ("    " ++) (showMapLines output))
+    ++ (showFinalLines final)
+
+showVerifyEquivLines :: Trans -> [String]
+showVerifyEquivLines t
+    | verifyEquiv t = []
+    | otherwise     = ["  WARN: transducer has wrong equivalence set"]
+
+showMismatchingStatesLines :: Trans -> [String]
+showMismatchingStatesLines t
+    | mis == [] = []
+    | otherwise = ["  WARN: transducer has mismatching states with keys: " ++ (show mis)]
+    where
+        mis = mismatchingStates t
+
+verifyEquiv :: Trans -> Bool
+verifyEquiv Trans {states, equiv} = (HashSet.fromList (HashMap.elems states)) == equiv
+
+mismatchingStates :: Trans -> [Int]
+mismatchingStates Trans {states} = filter checkState (HashMap.keys states)
+    where
+        checkState k = (sid (states HashMap.! k)) /= k
+
+showMapLines :: (Show k, Show v) => HashMap k v -> [String]
+showMapLines = (map showPair) . HashMap.toList
+    where
+        showPair (k, v) = (show k) ++ " -> " ++ (show v)
+
+showFinalLines :: (Show o) => Maybe o -> [String]
+showFinalLines Nothing = []
+showFinalLines (Just output) = ["  final with output " ++ (show output)]
+
+
+instance Hashable State where
+    hashWithSalt salt State {sid, transition, final, output}
+        = hashWithSalt salt (sid, transition, final, output)
+
+-- **** Data ****
+
 data State = State {
     sid :: Int,
     transition :: HashMap Char Int,
@@ -27,41 +98,8 @@ data Trans = Trans {
     start :: Int
 } deriving (Eq)
 
-instance Hashable State where
-    hashWithSalt salt State {sid, transition, final, output}
-        = hashWithSalt salt (sid, transition, final, output)
-
-instance Show Trans where
-    show = (foldr (++) "") . (map (++ "\n")) . showTransLines
-
-instance Show State where
-    show = (foldr (++) "") . (map (++ "\n")) . showStateLines
-
-showTransLines :: Trans -> [String]
-showTransLines Trans {states, start}
-    =  ["transducer with start " ++ (show start)]
-    ++ (map ("  " ++) (foldr (++) [] (map showStateLines (HashMap.elems states))))
-
-showStateLines :: State -> [String]
-showStateLines State {sid, transition, final, output}
-    =  ["state " ++ show sid ++ ":"]
-    ++ ["  transitions:"]
-    ++ (map ("    " ++) (showMapLines transition))
-    ++ ["  outputs:"]
-    ++ (map ("    " ++) (showMapLines output))
-    ++ (showFinalLines final)
-
-showMapLines :: (Show k, Show v) => HashMap k v -> [String]
-showMapLines = (map showPair) . HashMap.toList
-    where
-        showPair (k, v) = (show k) ++ " -> " ++ (show v)
-
-showFinalLines :: (Show o) => Maybe o -> [String]
-showFinalLines Nothing = []
-showFinalLines (Just output) = ["  final with output " ++ (show output)]
-
 juljul :: Trans
-juljul = Trans {
+juljul = updateEquiv $ Trans {
     start = 0,
     states = HashMap.fromList [
         (0, State 
